12d7b73c171c73d44af5a50fdf707404a65083e3
John Bauer <horatio@gmail.com>
2016-08-18 23:28:14 -0500

    Add a least squares regression and then print out the differences between the projections

--- bpms/bpms.py
+++ bpms/bpms.py
@@ -40,10 +40,13 @@ if __name__ == "__main__":
     lines = [line.split("#")[0] for line in lines]
     times = [float(line) for line in lines if line]
 
+    print "// BPMS THAT GO THROUGH THE GIVEN TIMES"
+
     current_beat = start_beat
+    beats = [current_beat]
     for index, (start, end) in enumerate(zip(times[:-1], times[1:])):
-        beats = round((end - start) / (60. / bpm) * 2) / 2.0
-        tempo = 60. / (end - start) * beats
+        interval = round((end - start) / (60. / bpm) * 2) / 2.0
+        tempo = 60. / (end - start) * interval
         if index == 0:
             print "#OFFSET:%.4f;" % (-times[0] + (60. / tempo) * current_beat)
             print "#BPMS:"
@@ -52,7 +55,29 @@ if __name__ == "__main__":
         # comment = " # %d" % start
         comment = ""
         print "%.1f=%.4f%s%s" % (display_beat, tempo, continuation, comment)
-        current_beat = current_beat + beats
+        current_beat = current_beat + interval
+        beats.append(current_beat)
 
+    # Now, perform LSR on the given times and the closest
+    # approximating beats.  That will give us an idea of whether or
+    # not the changes in the first half are necessary.
+    sum_x = sum(beats)
+    sum_y = sum(times)
+    sum_xx = sum(x * x for x in beats)
+    sum_xy = sum(x * y for (x, y) in zip(beats, times))
+    N = len(beats)
 
+    A = (N * sum_xy - sum_x * sum_y) / (N * sum_xx - sum_x * sum_x)
+    B = (sum_y * sum_xx - sum_x * sum_xy) / (N * sum_xx - sum_x * sum_x)
 
+    print
+    print
+    print "// OFFSET, BPM DETERMINED BY LSR"
+    print "#OFFSET:%.4f" % B
+    print "#BPMS:%.4f" % (60.0 / A)
+
+    print
+    print "// Errors between the fixed BPM and the times given"
+    projected_times = [B + A * x for x in beats]
+    for x, y in zip(projected_times, times):
+        print "%.3f %.3f %.3f" % (x, y, x - y)
