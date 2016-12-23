4d4fd0fb2209741f747ca072b75d336d2ddbf652
John Bauer <horatio@gmail.com>
2016-12-23 13:03:51 -0800

    Add a family of radio buttons for limiting which files to download

--- ziv/scrape_interface.py
+++ ziv/scrape_interface.py
@@ -2,10 +2,11 @@
 Done:
 have a menu listing the categories you can download (done)
 file picker to choose the directory to save to
-
-TODO:
 have a text field offering with a prefix you can limit to
 add a text field for regex as well
+
+TODO:
+make regex filtering a thing...
 button that launches the download
 button that reloads the categories
 
@@ -31,6 +32,8 @@ class App(tk.Tk):
 
         self.category_map = category_map
 
+        # Add a dropdown chooser for the platform
+        # eg Arcade, Wii, etc
         platform_list = list(category_map.keys())
         max_width = max(len(x) for x in platform_list)
         self.platform_var = tk.StringVar()
@@ -43,6 +46,9 @@ class App(tk.Tk):
         self.platform_drop.bind("<<ComboboxSelected>>", self.choose_platform)
         self.platform_drop.pack(anchor="w")
 
+        # Add a dropdown chooser for the category
+        # When the platform changes, the list of available choices
+        # will be updated
         category_list = list(category_map[platform_list[0]].keys())
         max_width = max(max(len(x) for x in platform.keys())
                         for platform in category_map.values())
@@ -56,15 +62,48 @@ class App(tk.Tk):
         self.category_drop.bind("<<ComboboxSelected>>", self.choose_category)
         self.category_drop.pack()
 
-        directory_chooser = tk.Button(self.frame,
-                                      text='askdirectory',
+        # Build a frame for the directory chooser
+        # Putting things in a small frame keeps them
+        # coordinated visually
+        # There is a button to show the chooser widget and a
+        # label which shows the current directory
+        directory_frame = tk.Frame(self.frame)
+        directory_chooser = tk.Button(directory_frame,
+                                      text='Destination directory',
                                       command=self.ask_directory)
-        directory_chooser.pack(anchor="w", side=tk.LEFT)
+        directory_chooser.pack(side=tk.LEFT)
         self.directory_var = tk.StringVar()
         self.directory_var.set(os.getcwd())
-        label = ttk.Label(self.frame, textvariable=self.directory_var)
+        label = ttk.Label(directory_frame, textvariable=self.directory_var)
         label.pack(side=tk.LEFT)
+        directory_frame.pack(anchor="w")
 
+        # Build a frame for the filters
+        # TODO: maybe make filter_frame a grid to ensure that
+        # prefix_entry and regex_entry align
+        filter_frame = tk.Frame(self.frame)
+        self.filter_choice = tk.IntVar(0)
+        radio_none = tk.Radiobutton(filter_frame, text="No filter",
+                                    variable=self.filter_choice, value=0)
+        radio_none.pack(anchor="w")
+
+        prefix_frame = tk.Frame(filter_frame)
+        radio_prefix = tk.Radiobutton(prefix_frame, text="Prefix",
+                                      variable=self.filter_choice, value=1)
+        radio_prefix.pack(anchor="w", side=tk.LEFT)
+        self.prefix_entry = tk.Entry(prefix_frame)
+        self.prefix_entry.pack()
+        prefix_frame.pack()
+
+        regex_frame = tk.Frame(filter_frame)
+        radio_regex = tk.Radiobutton(regex_frame, text="Regex",
+                                     variable=self.filter_choice, value=2)
+        radio_regex.pack(anchor="w", side=tk.LEFT)
+        self.regex_entry = tk.Entry(regex_frame)
+        self.regex_entry.pack()
+        regex_frame.pack()
+        filter_frame.pack(anchor="w")
+        
     def ask_directory(self):
         new_dir = tkFileDialog.askdirectory(parent=self.frame,
                                             title="Directory to save simfiles")
