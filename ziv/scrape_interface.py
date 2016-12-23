1eff3567e9caa0994cac304e59164c2cd1e89108
John Bauer <horatio@gmail.com>
2016-12-23 02:29:47 -0800

    Update the category picker to a combobox, which includes the ability to scroll

--- ziv/scrape_interface.py
+++ ziv/scrape_interface.py
@@ -9,6 +9,7 @@ advanced: a progress bar
 """
 
 import Tkinter as tk
+import ttk
 import scrape_category
 
 class App(tk.Tk):
@@ -27,11 +28,16 @@ class App(tk.Tk):
         self.platform_drop.pack(side=tk.LEFT)
 
         category_list = list(category_map[platform_list[0]].keys())
+        max_width = max(max(len(x) for x in platform.keys())
+                        for platform in category_map.values())
         self.category_var = tk.StringVar()
         self.category_var.set(category_list[0])
-        self.category_drop = tk.OptionMenu(frame, self.category_var,
-                                           *category_list,
-                                           command=self.choose_category)
+        self.category_drop = ttk.Combobox(frame,
+                                          textvariable=self.category_var,
+                                          values=category_list,
+                                          state="readonly",
+                                          width=max_width)
+        self.category_drop.bind("<<ComboboxSelected>>", self.choose_category)
         self.category_drop.pack(side=tk.LEFT)
 
         self.category_map = category_map
@@ -39,18 +45,13 @@ class App(tk.Tk):
     def choose_platform(self, platform):
         print "Platform updated to %s" % platform
         self.category_var.set('')
-        self.category_drop['menu'].delete(0, 'end')
         new_category_list = list(category_map[platform].keys())
-        for category in new_category_list:
-            command=tk._setit(self.category_var, category,
-                              self.choose_category)
-            self.category_drop['menu'].add_command(label=category,
-                                                   command=command)
+        self.category_drop['values'] = new_category_list
 
         self.category_var.set(new_category_list[0])
 
-    def choose_category(self, arg):
-        print arg
+    def choose_category(self, event):
+        print "%s: %s" % (self.platform_var.get(), self.category_var.get())
         # self.category_var.get(), self.platform_var.get()
 
 # TODO: this seems pretty fast, but we might want to cache the results
