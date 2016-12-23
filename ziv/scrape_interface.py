c85186ecb6b00c61c516b7c016da1622ec72793e
John Bauer <horatio@gmail.com>
2016-12-23 02:39:35 -0800

    Make the platform choice also a Combobox instead of an OptionMenu

--- ziv/scrape_interface.py
+++ ziv/scrape_interface.py
@@ -20,12 +20,16 @@ class App(tk.Tk):
         frame.pack()
 
         platform_list = list(category_map.keys())
+        max_width = max(len(x) for x in platform_list)
         self.platform_var = tk.StringVar()
         self.platform_var.set(platform_list[0])
-        self.platform_drop = tk.OptionMenu(frame, self.platform_var,
-                                           *platform_list,
-                                           command=self.choose_platform)
-        self.platform_drop.pack(side=tk.LEFT)
+        self.platform_drop = ttk.Combobox(frame,
+                                          textvariable=self.platform_var,
+                                          values=platform_list,
+                                          state="readonly",
+                                          width=max_width)
+        self.platform_drop.bind("<<ComboboxSelected>>", self.choose_platform)
+        self.platform_drop.pack(anchor="w")
 
         category_list = list(category_map[platform_list[0]].keys())
         max_width = max(max(len(x) for x in platform.keys())
@@ -38,16 +42,15 @@ class App(tk.Tk):
                                           state="readonly",
                                           width=max_width)
         self.category_drop.bind("<<ComboboxSelected>>", self.choose_category)
-        self.category_drop.pack(side=tk.LEFT)
+        self.category_drop.pack()
 
         self.category_map = category_map
         
-    def choose_platform(self, platform):
+    def choose_platform(self, event):
+        platform = self.platform_var.get()
         print "Platform updated to %s" % platform
-        self.category_var.set('')
         new_category_list = list(category_map[platform].keys())
         self.category_drop['values'] = new_category_list
-
         self.category_var.set(new_category_list[0])
 
     def choose_category(self, event):
